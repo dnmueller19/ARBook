53b9c199d775a7bc1d396be16d0ad0bf40b6cd54
Daniel Mueller <muellerdn@gmail.com>
2018-07-31 15:14:44 -0400

    reset, movement for ship added : movement for airplane and clouds added

---
+++ Assets/MoveScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour {
+	public float xvalue, yvalue, zvalue;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//vector 3 take x,y,z directions
+		transform.Translate(new Vector3(xvalue, yvalue, zvalue) * Time.deltaTime);
+	}
+}
---
+++ Assets/Reset.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset : MonoBehaviour {
+	Vector3 originalPosition;
+	// Use this for initialization
+	void Start () {
+		originalPosition = transform.localPosition;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//called when teh ship or plane its it 
+	private void OnTriggerEnter(Collider collide)
+	{
+		if (collide.gameObject.CompareTag("ShipWall")) {
+			transform.localPosition = originalPosition;
+		}
+
+	}
+}
